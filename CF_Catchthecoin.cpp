29e4fb9475ee4cf170b75a35fd90e3d844addcfa
VatsalS31 <120210435+VatsalS31@users.noreply.github.com>
2024-12-12 21:38:45 +0530

    Create CF_Catchthecoin.cpp

---
+++ CF_Catchthecoin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(y>=-1)
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+        }
+    }
+}
